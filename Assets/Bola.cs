07cf8c2a09c0fe3eebd3cad500f1bda2b60924e2
tl-almeida <thiagolimadealmeid4@gmail.com>
2020-10-25 15:21:20 -0300

    initial commit

---
+++ Assets/Bola.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    public float Speed = 5f;
+    void Start()
+    {
+        float SpeedX = Random.Range(0,2) == 0 ? -1 : 1;
+        float SpeedY = Random.Range(0,2) == 0 ? -1 : 1;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(Speed * SpeedX, Speed * SpeedY, 0f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Plataforma.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour
+{
+    public float Speed = 5f;
+    public bool IsRight;
+    void Update()
+    {
+        if (IsRight) 
+        {
+            transform.Translate(0f, Input.GetAxis("Vertical") * Speed * Time.deltaTime, 0f);
+        }
+        else
+        {
+            transform.Translate(0f, Input.GetAxis("Vertical2") * Speed * Time.deltaTime, 0f);
+        }
+    }
+}
